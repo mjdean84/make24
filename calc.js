eda72a9353a67eab5efe5e0a50d1a3224d1545c8
mjdean84 <43919438+mjdean84@users.noreply.github.com>
2019-02-04 20:02:23 -0500

    Update and rename calcFunction.js to calc.js

---
+++ calc.js
@@ -0,0 +1,39 @@
+
+var total;
+
+function calc() {
+    total = firstCard;
+    if (oper1 != null && secondCard != null) {
+        arith(oper1, secondCard);
+
+        if (oper2 != null && thirdCard != null) {
+            arith(oper2, thirdCard);
+
+            if (oper3 != null && fourthCard != null) {
+                arith(oper3, fourthCard);
+
+                if (total == 24) {
+                    $("#result").html("YOU'RE RIGHT!!!").css("color", "lightgreen");
+                } else {
+                    $("#result").html("NOPE, TRY AGAIN").css("color", "red");
+                }
+            }
+        }
+        $("#ans").html(" = " + total);
+    }
+}
+
+function arith(oper, num) {
+    if (oper == "+") {
+        total = total + num;
+    }
+    else if (oper == "-") {
+        total = total - num;
+    }
+    else if (oper == "x") {
+        total = total * num;
+    }
+    else {
+        total = total / num;
+    }
+}

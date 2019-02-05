432248fa918cc93c9bb6daf9ccef002a9eb589a2
mjdean84 <43919438+mjdean84@users.noreply.github.com>
2019-02-04 20:07:19 -0500

    Update and rename findSolutionsFunction.js to findSolutions.js

--- findSolutions.js
+++ findSolutions.js
@@ -8,9 +8,10 @@ $(document).ready(function () {
     });
 });
 
-function findSolution() {
-    
-    var num1=0, num2=0, num3=0, num4=0, op1=0, op2=0, op3=0;
+function findSolutions() {
+    solutions = "";
+    var numbers = [card1.value, card2.value, card3.value, card4.value];
+    var num1 = 0, num2 = 0, num3 = 0, num4 = 0, op1 = 0, op2 = 0, op3 = 0;
     for (num1 = 0; num1 < 4; num1++) {
         for (num2 = 0; num2 < 4; num2++) {
             if (num2 == num1) continue;
@@ -20,12 +21,15 @@ function findSolution() {
                 for (op1 = 0; op1 < 4; op1++) {
                     for (op2 = 0; op2 < 4; op2++) {
                         for (op3 = 0; op3 < 4; op3++) {
-                            if (checkSolution(numbers[num1], numbers[num2], numbers[num3], numbers[num4], op1, op2, op3) == 24) {
-                                if (solution.includes(numbers[num1] + "" + showOp(op1) + "" + numbers[num2] + "" + showOp(op2) + "" +
-                                    numbers[num3] + "" + showOp(op3) + "" + numbers[num4])) continue;
-                                solution += numbers[num1] + "" + showOp(op1) + "" + numbers[num2] + "" + showOp(op2) + "" +
+                            if (checkSol(checkSol(checkSol(numbers[num1], op1, numbers[num2]), op2,
+                                numbers[num3]), op3, numbers[num4]) == 24) {
+
+                                solution = numbers[num1] + "" + showOp(op1) + "" + numbers[num2]
+                                    + "" + showOp(op2) + "" +
                                     numbers[num3] + "" + showOp(op3) + "" + numbers[num4] + "<br>";
-                                
+
+                                if (solutions.includes(solution)) continue;
+                                solutions += solution;
                             }
                         }
                     }
@@ -33,17 +37,10 @@ function findSolution() {
             }
         }
     }
-    if (solution == "") dealCards();
-    
+    if (solutions == "") dealCards();
 }
 
-
-
-function checkSolution(num1, num2, num3, num4, op1, op2, op3) {
-    return findOps(findOps(findOps(num1, op1, num2), op2, num3), op3, num4);
-}
-
-function findOps(num1, op, num2) {
+function checkSol(num1, op, num2) {
     switch (op) {
         case 0:
             return num1 + num2;
